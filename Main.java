cd899461e63118b6e54a4bbe839aca7961686c30
Faseeh-Naqvi <faseehnaqvi100@gmail.com>
2023-08-10 19:08:47 -0400

    -Uncommented the main portion of the program -fixed up the move() method logic -need to fix display method

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
-        hanoi h = new hanoi();
+
         Scanner s=new Scanner(System.in);
         System.out.print("Enter the number of disks: ");
-        System.out.println();
         int n=s.nextInt();
-
-        h.hanoi(n,1,3,2);
+        System.out.println();
+        hanoi h = new hanoi(n);
         System.out.println("It took: "+h.numMoves+" moves to solve the puzzle");
+
+
+//        h.hanoi(n,1,3,2);
+//        System.out.println("It took: "+h.numMoves+" moves to solve the puzzle");
     }
 }
--- hanoi.java
+++ hanoi.java
@@ -6,79 +6,89 @@ class hanoi {
      * Using stacks actually moving the information.
      */
 
-    /**
+
     tower tower1 = new tower();
     tower tower2 = new tower();
     tower tower3 = new tower();
+    int start =1;
+    int end=3;
 
 
-    public <E> void Hanoi(int n, int start, int end) {
+
+    public hanoi(int n) {
 
         tower1.setTowerNum(1);
         tower2.setTowerNum(2);
         tower3.setTowerNum(3);
 
 
-    if(start==1){//pushes the numbers that represent the blocks in the tower. least at top and greatest at bottom.
+    //pushes the numbers that represent the blocks in the tower. least at top and greatest at bottom.
         for (int i = n; i>0;i--){
             tower1.push(i);
         }
 
-    }
-
-    if(start==2){//pushes the numbers that represent the blocks in the tower. least at top and greatest at bottom.
-        for (int i = n; i>0;i--){
-            tower2.push(i);
-        }
-    }
-
-    if(start==3){//pushes the numbers that represent the blocks in the tower. least at top and greatest at bottom.
-        for (int i = n; i>0;i--){
-            tower3.push(i);
-        }
-    }
-
     move(n,tower1,tower2,tower3);
+    tower1.display();
+    tower2.display();
+    tower3.display();
+
+
+
+
+
 
     }
 
+    int numMoves=0;
     public void move(int n, tower start,tower other, tower end){
-        String moveList="";
 
         if(n==1){
             end.push(start.pop());
-            moveList= moveList+start.getTowerNum()+"-->"+end.getTowerNum()+" ";
+            //System.out.println("Move Disc from "+start.getTowerNum()+" --> "+end.getTowerNum()+"\n");
+            numMoves++;
+        }
+        else {
+            move((n-1),start,end,other);
+            end.push(start.pop());
+            //System.out.println("Move Disc from "+start.getTowerNum()+" --> "+end.getTowerNum()+"\n");
+            numMoves++;
+            move((n-1),end,other,start);
         }
 
 
     }
 
 
-     *\
+
+
+
+
+
+
+
 
     /**
      * Using print, just saying what moves to take.
      */
-
-    int numMoves=0;
-
-    public void hanoi(int n, int start,int end, int mid){
-        if(n==1){
-            System.out.println("Move Disc from "+start+" --> "+end+"\n");
-            numMoves++;
-        }
-        else{
-            hanoi((n-1),start,mid,end);
-            System.out.println("Move Disc from "+start+" --> "+end+"\n");
-            hanoi((n-1),mid,end,start);
-            numMoves++;
-        }
-
-
-    }
-
-
-
+//    int numMoves=0;
+//
+//    public void hanoi(int n, int start,int mid, int end){
+//        if(n==1){
+//            System.out.println("Move Disc from "+start+" --> "+end+"\n");
+//            numMoves++;
+//        }
+//        else{
+//            hanoi((n-1),start,end,mid);
+//            System.out.println("Move Disc from "+start+" --> "+end+"\n");
+//            hanoi((n-1),end,mid,start);
+//            numMoves++;
+//        }
+//
+//
+//    }
+//
+//
+//
 
 
 }
